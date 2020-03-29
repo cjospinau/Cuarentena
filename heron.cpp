63a68207464f037dc1ac6ae80e6838df2e3aebe4
Slackware Live User <live@darkstar.example.net>
2020-03-28 18:43:40 -0700

    Gráfica del error para el área de un triángulo

--- heron.cpp
+++ heron.cpp
@@ -4,19 +4,21 @@
 double triangle(double a, double b, double A);
 double Heron1(double a, double b, double c);
 double Heron2(double a, double b, double c);
+double error(double a, double b, double c);
+
 
 int main(void)
 {
   double pi = std::acos(-1);
   double A;
-  double a = 4;
-  double b = 3;
+  double a = 0.5;
+  double b = 0.1;
   
  
-  for(A = pi/12; A < pi; A = A + pi/12)
+  for(A = pi/36; A < pi; A = A + pi/36)
    {
    double c = triangle(a,b,A);
-   std::cout << A*(180/pi) << "\t" << triangle(a,b,A) << "\t" << Heron1(a,b,c) << "\t" << Heron2(a,b,c) <<  std::endl;
+   std::cout << A*(180/pi) << "\t" << triangle(a,b,A) << "\t" << Heron1(a,b,c)<< "\t" << Heron2(a,b,c) << "\t" << error(a,b,c) << "\t" <<  std::endl;
    }
 
    return 0;
@@ -41,6 +43,15 @@ double Heron1(double a, double b, double c)
 double Heron2(double a, double b, double c)
 {
   double S2;
-  S2 = (1.0/4.0)*std::sqrt((a+b+c)*(c-a+b)*(c+a+b)*(a+b-c));
+  S2 =1.0/4*std::sqrt((a+b+c)*(c-a+b)*(c+a+b)*(a+b-c));
   return S2;
 }
+
+double error(double a, double b, double c)
+{
+  double dif;
+  dif =  std::abs((Heron1(a,b,c) - Heron2(a,b,c)))/Heron1(a,b,c);
+  return dif;
+}
+
+
